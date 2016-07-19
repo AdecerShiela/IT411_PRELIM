cc1e92d83725f1d61a220cbf9a2cc266e60e94e1
mariamonica <monicamicabalo@gmail.com>
2016-07-19 19:48:21 +0800

    PRELIM

---
+++ newgrade.js
@@ -0,0 +1,8 @@
+
+  function addyears(grade,increment){
+ var grade = grade + increment;
+ if(grade<75){
+    console.log("After " + increment + "years: " + 'I dont have to think about my life yet');
+}
+   
+ }
